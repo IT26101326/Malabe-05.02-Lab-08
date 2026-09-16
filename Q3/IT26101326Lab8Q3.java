e8251638eba6a424b21176bffb7f821530c1bff2
IT26101326 <IT26101326@my.sliit.lk>
2026-09-16 20:56:37 +0530

    Add Lab8Q3.java to Q3

---
+++ Q3/IT26101326Lab8Q3.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class IT26101326Lab8Q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[6];
+        
+        int i = 0;
+        while (i < 6) {
+            System.out.print("Enter a Positive Number (" + (i + 1) + "/6): ");
+            int num = sc.nextInt();
+            
+            if (num <= 0) {
+                System.out.println("Error: Please Enter ONLY Positive Numbers");
+            } else {
+                numbers[i] = num;
+                i++;   // valid number-ஆ இருந்தா தான் அடுத்த position-க்கு போகும்
+            }
+        }
+        
+        System.out.println();
+        System.out.println("Array Contents:");
+        for (int j = 0; j < 6; j++) {
+            System.out.print(numbers[j] + " ");
+        }
+        
+        // Find maximum
+        int max = numbers[0];
+        for (int j = 1; j < 6; j++) {
+            if (numbers[j] > max) {
+                max = numbers[j];
+            }
+        }
+        
+        System.out.println();
+        System.out.println("The Maximum Number Entered: " + max);
+    }
+}
